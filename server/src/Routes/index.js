84a78354aed3bd282407f4342624e737ef000259
Miguel ACH <ayalachavezmiguel@gmail.com>
2023-08-12 21:08:09 -0400

    Se actualizo las rutas, antes eran 5 ahora solo son 3

--- server/src/Routes/index.js
+++ server/src/Routes/index.js
@@ -2,20 +2,11 @@ const { Router } = require("express");
 const mainRouter = Router();
 
 const characterRouter = require("./characterRouter");
-const loginRouter = require("./loginRouter");
 const favoriteRouter = require("./favoriteRouter");
 const userRouter = require("./userRouter");
 
 mainRouter.use("/characters", characterRouter);
-
 mainRouter.use("/favorite", favoriteRouter);
-mainRouter.use("/deleteFav", favoriteRouter);
-
-mainRouter.use("/login", loginRouter);
 mainRouter.use("/user", userRouter);
 
 module.exports = mainRouter;
-
-//user => login
-//favorite => favorite, deleteFav
-//character
