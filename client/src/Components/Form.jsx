34a633ccd0fdf05a2b708c87659e1561b210eeff
Miguel ACH <ayalachavezmiguel@gmail.com>
2023-09-04 10:35:40 -0400

    Texto login muestra boton re registro y registro muestra el boton login

--- client/src/Components/Form.jsx
+++ client/src/Components/Form.jsx
@@ -1,15 +1,21 @@
+// HOOK´S
 import { useState } from "react";
 import validation from "../Utils/validation";
-import "../StyleSheets/Form.css"
+import "../StyleSheets/Form.css";
 
 const Form = ({ login }) => {
-
   const [userData, setUserData] = useState({
     email: "",
     password: "",
   });
 
   const [errors, setErrors] = useState({});
+
+  const [userOption, setUserOption] = useState("Login");
+
+  const handleOption = () => {
+    userOption === "Login" ? setUserOption("Check In") : setUserOption("Login");
+  };
 
   const handleChange = (event) => {
     setUserData({
@@ -22,44 +28,63 @@ const Form = ({ login }) => {
   };
 
   const handleSubmit = (event) => {
-    event.preventDefault();
-    login(userData);
+    const optionButton = event.target.name;
+    if (optionButton === "login") {
+      event.preventDefault();
+      login(userData);
+    } else {
+      alert("registro");
+      event.preventDefault();
+    }
   };
 
   return (
-    <form className="form-component" >
-        <img
-          className="logo"
-          src={require(`../image/Rick-And-Morty.png`)}
-          alt="Login Rick"
-        />
-        <label className="texto" htmlFor="email">
-          Email:
-        </label>
-        <input
-          type="text"
-          value={userData.email}
-          name="email"
-          onChange={handleChange}
-          placeholder="abc@gmail.com"
-          required
-        />
-        {errors.email && <p className="error">{errors.email}</p>}
+    <form className="form-component">
+      <img
+        className="logo"
+        src={require(`../image/Rick-And-Morty.png`)}
+        alt="Login Rick"
+      />
+      <label className="texto" htmlFor="email">
+        Email:
+      </label>
+      <input
+        type="text"
+        value={userData.email}
+        name="email"
+        onChange={handleChange}
+        placeholder="abc@gmail.com"
+        required
+      />
+      {errors.email && <p className="error">{errors.email}</p>}
 
-        <label className="texto" htmlFor="password">
-          Password
-        </label>
-        <input
-          type="password"
-          value={userData.password}
-          name="password"
-          onChange={handleChange}
-          placeholder="*************"
-          required
-          maxLength="10"
-        />
-        {errors.password && <p className="error">{errors.password}</p>}
-        <button className="btn-envio" onClick={handleSubmit}>Submit</button>
+      <label className="texto" htmlFor="password">
+        Password
+      </label>
+      <input
+        type="password"
+        value={userData.password}
+        name="password"
+        onChange={handleChange}
+        placeholder="*************"
+        required
+        maxLength="10"
+      />
+      {errors.password && <p className="error">{errors.password}</p>}
+      {userOption === "Check In" ? (
+        <button className="btn-envio" name="login" onClick={handleSubmit}>
+          {" "}
+          Login{" "}
+        </button>
+      ) : (
+        <button className="btn-registro" name="checkIn" onClick={handleSubmit}>
+          {" "}
+          Check in{" "}
+        </button>
+      )}
+      <p className="text-option" onClick={handleOption}>
+        {userOption}
+      </p>
     </form>
   );
 };
