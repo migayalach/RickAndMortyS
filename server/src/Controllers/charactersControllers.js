d157769f9cb3f40cac0622aa7e00223c9ff1a09d
Miguel ACH <ayalachavezmiguel@gmail.com>
2023-09-05 23:09:57 -0400

    Busca por nombre o id

--- server/src/Controllers/charactersControllers.js
+++ server/src/Controllers/charactersControllers.js
@@ -1,21 +1,40 @@
 const axios = require("axios");
 const URL = `https://rickandmortyapi.com/api/character`;
 
-const getCharacter = async (id) => {
-  if (id < 867) {
-    const charcterApi = (await axios.get(`${URL}/${id}`)).data;
-    const obj = {
-      id: charcterApi.id,
-      name: charcterApi.name,
-      gender: charcterApi.gender,
-      species: charcterApi.species,
-      origin: charcterApi.origin,
-      image: charcterApi.image,
-      status: charcterApi.status,
-    };
+const getCharacter = async (dataType, id) => {
+  if (dataType === "number") {
+    if (+id < 867) {
+      const charcterApi = (await axios.get(`${URL}/${+id}`)).data;
+      const obj = {
+        id: charcterApi.id,
+        name: charcterApi.name,
+        gender: charcterApi.gender,
+        species: charcterApi.species,
+        origin: charcterApi.origin,
+        image: charcterApi.image,
+        status: charcterApi.status,
+      };
+      return obj;
+    }
+    throw Error(`La API solo cuenta con 826 personajes`);
+  } else {
+    // OJO ESTO VEREMOS EN EL FRONT
+    const charcterApi = (await axios.get(`${URL}/?name=${id}`)).data.results;
+    const obj = charcterApi.map(
+      ({ id, name, gender, species, origin, image, status }) => {
+        return {
+          id,
+          name,
+          gender,
+          species,
+          origin,
+          image,
+          status,
+        };
+      }
+    );
     return obj;
   }
-  throw Error(`La API solo cuenta con 826 personajes`);
 };
 
 module.exports = {
