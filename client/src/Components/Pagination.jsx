c94fd85e6820bb566eccefad50bc390cdf8a217b
Miguel ACH <ayalachavezmiguel@gmail.com>
2023-09-22 20:28:46 -0400

    paginacion listo

--- client/src/Components/Pagination.jsx
+++ client/src/Components/Pagination.jsx
@@ -1,7 +1,60 @@
-const Pagination = () => {
+const Pagination = ({
+  dataLength,
+  itemsPerPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const totalPages = Math.ceil(dataLength / itemsPerPage);
+
+  const handlePageClick = (pageNumber) => {
+    onPageChange(pageNumber);
+  };
+
+  const handlePrevClick = () => {
+    if (currentPage > 1) {
+      onPageChange(currentPage - 1);
+    }
+  };
+
+  const handleNextClick = () => {
+    if (currentPage < totalPages) {
+      onPageChange(currentPage + 1);
+    }
+  };
+
+  const renderPageNumbers = () => {
+    const pageNumbers = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(
+        <span
+          key={i}
+          onClick={() => handlePageClick(i)}
+          style={{
+            cursor: "pointer",
+            fontWeight: currentPage === i ? "bold" : "normal",
+          }}
+        >
+          {i}
+        </span>
+      );
+    }
+    return pageNumbers;
+  };
+
   return (
     <div>
-      <h1>HOlis :D</h1>
+      <h1>Pagination</h1>
+      {currentPage > 1 && (
+        <span onClick={handlePrevClick} style={{ cursor: "pointer" }}>
+          {"<Prev>"}
+        </span>
+      )}
+      <div>{renderPageNumbers()}</div>
+      {currentPage < totalPages && (
+        <span onClick={handleNextClick} style={{ cursor: "pointer" }}>
+          {"Next"}
+        </span>
+      )}
     </div>
   );
 };
