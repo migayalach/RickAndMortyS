1810af555aa7e7f77c78a2cbb121a9d211ae0b52
migayalach <ayalachavezmiguel@gmail.com>
2023-11-21 20:57:44 -0400

    se cambio el nombre a algo mas descriptivo

---
+++ server/src/Middleware/toolsMiddleware.js
@@ -0,0 +1,17 @@
+const verityId = (request, response, next) => {
+  const { id, idUser } = request.params;
+  try {
+    if (!Number.isInteger(+id || +idUser)) {
+      throw Error`El tipo de dato que introdujo no es valido`;
+    }
+    return next();
+  } catch (error) {
+    return response
+      .status(400)
+      .json({ searchPerson: false, error: error.message });
+  }
+};
+
+module.exports = {
+  verityId,
+};
