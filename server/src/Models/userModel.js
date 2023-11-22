f1c31b8c484eeb411797aff5afb6938e52240956
migayalach <ayalachavezmiguel@gmail.com>
2023-11-21 20:38:28 -0400

    modelo listo para user

--- server/src/Models/userModel.js
+++ server/src/Models/userModel.js
@@ -12,7 +12,7 @@ module.exports = (sequelize) => {
       email: {
         type: DataTypes.STRING,
         allowNull: false,
-        // unique: true,
+        unique: true,
       },
       password: {
         type: DataTypes.STRING,
