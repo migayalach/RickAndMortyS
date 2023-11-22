ddda33fd2c03db06a6a54024a02c03c2c3d7c96b
migayalach <ayalachavezmiguel@gmail.com>
2023-11-21 20:41:29 -0400

    crud listo

--- server/src/Controllers/userControllers.js
+++ server/src/Controllers/userControllers.js
@@ -1,10 +1,57 @@
 const { User } = require("../DataBase/dataBase");
+const { Op } = require("sequelize");
+
 const postUser = async ({ email, password }) => {
   const user = await User.findOne({ where: { email } });
   if (!user) {
-    return await User.create({ email, password });
+    await User.create({ email, password });
+    return { create: true };
   }
   throw Error(`El email: ${email} ya se encuentra registrado`);
+};
+
+const getIdUser = async (idUser) => {
+  const userData = [await User.findOne({ where: { id: idUser } })];
+  if (userData) {
+    return userData.map(({ id, email }) => ({ id, email }));
+  }
+  throw Error(`No se pudo encontrar ningun usuario`);
+};
+
+const getUserName = async (email) => {
+  const userData = await User.findAll({
+    where: { email: { [Op.iLike]: `%${email}%` } },
+  });
+  if (userData.length) {
+    return userData.map(({ id, email }) => ({ id, email }));
+  }
+  throw Error(`El email: ${email} no existe`);
+};
+
+const getAllUser = async () => {
+  const usersData = await User.findAll();
+  if (usersData.length) {
+    return usersData.map(({ id, email }) => ({ id, email }));
+  }
+  throw Error(`No hay datos disponibles`);
+};
+
+const putUser = async (idUser, email, password) => {
+  const updateUser = await User.findOne({ where: { id: idUser } });
+  if (!updateUser) {
+    throw Error(`El email: ${email}, no se encuentra registrado`);
+  }
+  await User.update({ email, password }, { where: { id: idUser } });
+  return await getIdUser(idUser);
+};
+
+const userDetele = async (idUser) => {
+  const userData = await User.findOne({ where: { id: idUser } });
+  if (!userData) {
+    throw Error(`El email: ${email}, no se encuentra registrado`);
+  }
+  await User.destroy({ where: { id: idUser } });
+  return await getAllUser();
 };
 
 const getLogin = async ({ email, password }) => {
@@ -33,4 +80,13 @@ const searchInfo = async ({ email }) => {
   return { infoUser: result.id };
 };
 
-module.exports = { postUser, getLogin, searchInfo };
+module.exports = {
+  postUser,
+  getIdUser,
+  getUserName,
+  getAllUser,
+  getLogin,
+  putUser,
+  userDetele,
+  searchInfo,
+};
