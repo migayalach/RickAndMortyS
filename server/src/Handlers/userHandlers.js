bc5c9787a7c5cffdbe765c38624f22e2d3dcfc44
migayalach <ayalachavezmiguel@gmail.com>
2023-11-21 20:40:34 -0400

    logica para handler user implementada con exito

--- server/src/Handlers/userHandlers.js
+++ server/src/Handlers/userHandlers.js
@@ -2,18 +2,75 @@ const SUCCESS = 200;
 const ERROR = 500;
 const {
   postUser,
+  getIdUser,
+  getUserName,
+  getAllUser,
   getLogin,
+  putUser,
+  userDetele,
   searchInfo,
 } = require("../Controllers/userControllers");
 
-const userHandlers = async (request, response) => {
-  //agragar middleware para ver si se recive email y password
+const createUser = async (request, response) => {
   const { email, password } = request.body;
   try {
     const newUser = await postUser({ email, password });
-    response.status(SUCCESS).json({ create: true, newUser });
+    response.status(SUCCESS).json({ newUser });
   } catch (error) {
-    response.status(ERROR).json({ error: error.message });
+    response.status(ERROR).json({ create: false, error: error.message });
+  }
+};
+
+// ADMIN PUEDE VER LA LISTA DE USUARIOS por id, nombre o todo
+const getUserId = async (request, response) => {
+  const { idUser } = request.params;
+  try {
+    const dataResponse = await getIdUser(+idUser);
+    response.status(SUCCESS).json({ getUserId: true, dataResponse });
+  } catch (error) {
+    response.status(ERROR).json({
+      getUserId: false,
+      error: error.message,
+      message: "No se pudo encontrar ningun usuario",
+    });
+  }
+};
+const getNameUser = async (request, response) => {
+  const { email } = request.query;
+  try {
+    const dataResponse = email ? await getUserName(email) : await getAllUser();
+    response.status(SUCCESS).json({ getNameUser: true, dataResponse });
+  } catch (error) {
+    response.status(ERROR).json({ getNameUser: false, error: error.message });
+  }
+};
+
+const updateUser = async (request, response) => {
+  const { idUser, email, password } = request.body;
+  try {
+    const userUpdate = await putUser(idUser, email, password);
+    response.status(SUCCESS).json({
+      updateUser: true,
+      userUpdate,
+      message: "Usuaro actualizado con exito",
+    });
+  } catch (error) {
+    response.status(ERROR).json({ updateUser: false, error: error.message });
+  }
+};
+
+// SOLO EL ADMIN PUEDE ELIMINAR CUENTAS
+const deleteUser = async (request, response) => {
+  const { idUser } = request.params;
+  try {
+    const responseData = await userDetele(idUser);
+    response.status(SUCCESS).json({
+      deleteUser: true,
+      responseData,
+      message: "Usuaro eliminado con exito",
+    });
+  } catch (error) {
+    response.status(ERROR).json({ deleteUser: false, error: error.message });
   }
 };
 
@@ -32,4 +89,11 @@ const loginHandlers = async (request, response) => {
   }
 };
 
-module.exports = { userHandlers, loginHandlers };
+module.exports = {
+  createUser,
+  getUserId,
+  getNameUser,
+  updateUser,
+  deleteUser,
+  loginHandlers,
+};
