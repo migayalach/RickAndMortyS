40b01cd2489b1bbe49e08379c3d8f9ceac24f7c1
migayalach <ayalachavezmiguel@gmail.com>
2023-11-21 20:39:14 -0400

    middleware implementado para user

---
+++ server/src/Middleware/userMiddleware.js
@@ -0,0 +1,20 @@
+const { emailRegex } = require("../Utils/regex");
+
+const postUserMiddleware = (request, response, next) => {
+  const { email, password } = request.body;
+  try {
+    if (!emailRegex.test(email)) {
+      throw Error(`Introduzca una direccion email valida`);
+    }
+    if (password.length < 1) {
+      throw Error(`El campo password no puede estar vacio`);
+    }
+    return next();
+  } catch (error) {
+    response.status(400).json({ user: "false", error: error.message });
+  }
+};
+
+module.exports = {
+  postUserMiddleware,
+};
