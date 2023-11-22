a3bc5da4448e7dbc41aaec34ddc863f2ef9e9966
migayalach <ayalachavezmiguel@gmail.com>
2023-11-21 20:35:11 -0400

    rutas listas

--- server/src/Routes/index.js
+++ server/src/Routes/index.js
@@ -4,9 +4,11 @@ const mainRouter = Router();
 const characterRouter = require("./characterRouter");
 const favoriteRouter = require("./favoriteRouter");
 const userRouter = require("./userRouter");
+const levelRouter = require("./levelRouter");
 
-mainRouter.use("/characters", characterRouter);
+mainRouter.use("/characters", characterRouter); //OK
+mainRouter.use("/user", userRouter); //OK
 mainRouter.use("/favorite", favoriteRouter);
-mainRouter.use("/user", userRouter);
+mainRouter.user("level", levelRouter);
 
 module.exports = mainRouter;
