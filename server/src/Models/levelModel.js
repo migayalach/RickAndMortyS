7c92717996be38b6b31db87b902a1be026e14a29
migayalach <ayalachavezmiguel@gmail.com>
2023-11-22 21:55:03 -0400

    se agrego timestamps, para no ver datos para auditoria

--- server/src/Models/levelModel.js
+++ server/src/Models/levelModel.js
@@ -1,16 +1,22 @@
 const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
-  sequelize.define("Level", {
-    idLevel: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      unique: true,
-      primaryKey: true,
+  sequelize.define(
+    "Level",
+    {
+      idLevel: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        unique: true,
+        primaryKey: true,
+      },
+      level: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
     },
-    level: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
-    },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
