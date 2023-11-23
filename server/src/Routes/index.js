dba101abc6bdbc92e2fbb451e05bc0b98815be68
migayalach <ayalachavezmiguel@gmail.com>
2023-11-23 12:30:04 -0400

    login

--- server/src/Routes/index.js
+++ server/src/Routes/index.js
@@ -5,10 +5,12 @@ const characterRouter = require("./characterRouter");
 const favoriteRouter = require("./favoriteRouter");
 const userRouter = require("./userRouter");
 const levelRouter = require("./levelRouter");
+const loginRouter = require("./loginRouter");
 
 mainRouter.use("/characters", characterRouter);
 mainRouter.use("/user", userRouter);
 mainRouter.use("/favorite", favoriteRouter);
 mainRouter.use("/level", levelRouter);
+mainRouter.use("/login", loginRouter);
 
 module.exports = mainRouter;
