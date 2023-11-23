a96cd01b7d1dd2931a698cc8dafb7e344cf8daec
migayalach <ayalachavezmiguel@gmail.com>
2023-11-23 09:42:55 -0400

    entry point'slisto, solo falta middlewares

--- server/src/Routes/levelRouter.js
+++ server/src/Routes/levelRouter.js
@@ -1,3 +1,17 @@
 const { Router } = require("express");
+const {
+  createLevel,
+  getLevelId,
+  getNameLevel,
+  editLevel,
+  deleteLevel,
+} = require("../Handlers/levelHandlers");
 const levelRouter = Router();
+
+levelRouter.post("/", createLevel);
+levelRouter.get("/:idLevel", getLevelId);
+levelRouter.get("/", getNameLevel);
+levelRouter.put("/", editLevel);
+levelRouter.delete("/:idLevel", deleteLevel);
+
 module.exports = levelRouter;
