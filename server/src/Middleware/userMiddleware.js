aca9b85c876af266f1cfc75c115076e11009dac8
migayalach <ayalachavezmiguel@gmail.com>
2023-11-24 22:30:23 -0400

    se quito password

--- server/src/Middleware/userMiddleware.js
+++ server/src/Middleware/userMiddleware.js
@@ -1,14 +1,17 @@
 const { emailRegex } = require("../Utils/regex");
 
 const postUserMiddleware = (request, response, next) => {
-  const { email, password } = request.body;
+  const {
+    email,
+    // , password
+  } = request.body;
   try {
     if (!emailRegex.test(email)) {
       throw Error(`Introduzca una direccion email valida`);
     }
-    if (password.length < 1) {
-      throw Error(`El campo password no puede estar vacio`);
-    }
+    // if (password.length < 1) {
+    //   throw Error(`El campo password no puede estar vacio`);
+    // }
     return next();
   } catch (error) {
     response.status(400).json({ user: "false", error: error.message });
