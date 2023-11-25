e6bb99f6d3f340022bf916ce1590b1cfeddb8198
migayalach <ayalachavezmiguel@gmail.com>
2023-11-24 22:19:54 -0400

    se reestructuro y se volvio a lo anterior

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -1,24 +1,21 @@
-// COMPONET'S
 import React from "react";
 import SearchBar from "./SearchBar";
-
-// HOOK'S
-import { useSelector } from "react-redux";
-
-// LIBRARY
-// REDUX
-// JAVASCRIP
-// STYLESHEET'S
+import { NavLink } from "react-router-dom";
 
 const Nav = ({ onSearch, logout }) => {
-  const { user } = useSelector((state) => state.infoUser);
   return (
     <div>
-      <SearchBar onSearch={onSearch} logout={logout} />
-      <div>
-        <p>
-          Bienvenid@: <span>{user}</span>
-        </p>
+      <SearchBar onSearch={onSearch} logout={logout}/>
+      <div className="button-nav">
+        <button className="aceptar-nav">
+          <NavLink className="nav-link" to="/about">About</NavLink>
+        </button>
+        <button className="aceptar-nav">
+          <NavLink className="nav-link" to="/home">Home</NavLink>
+        </button>
+        <button className="aceptar-nav">
+          <NavLink className="nav-link" to="/favorites">Favorites</NavLink>
+        </button>
       </div>
     </div>
   );
