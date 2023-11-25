166c9f08ea4fe16cf7efb7e45a3dd0195b429eab
migayalach <ayalachavezmiguel@gmail.com>
2023-11-24 22:08:25 -0400

    se borro ID_USER y se crero create user

--- client/src/Redux/action-types.js
+++ client/src/Redux/action-types.js
@@ -1,9 +1,10 @@
+export const LOGIN_USER = "LOGIN_USER";
+export const CREATE_USER = "CREATE_USER";
 export const ADD_FAV = "ADD_FAV";
 export const REMOVE_FAV = "REMOVE_FAV";
+
 export const FILTER_CARDS = "FILTER_CARDS";
 export const ORDER_CARDS = "ORDER_CARDS";
 export const GET_CHARACTERS = "GET_CHARACTERS";
 export const GET_ALL_FAVORITES = "GET_ALL_FAVORITES";
-export const ID_USER = "ID_USER";
 export const GET_NAME_CHARACTER = "GET_NAME_CHARACTER";
-export const LOGIN_USER = "LOGIN_USER";
