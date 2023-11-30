acd5ca2f4fbf4ea8279cc562031741f62ea680c8
migayalach <ayalachavezmiguel@gmail.com>
2023-11-29 20:01:14 -0400

    app.js restructurado

--- client/src/App.js
+++ client/src/App.js
@@ -1,143 +1,55 @@
-import { useState, useEffect } from "react";
+// COMPONENTS
+import { About, Favorites, Detail, Home, Login, Error, User } from "./Views";
+import NavBar from "./Components/NavBar";
+
+// HOOK'S
+import { useEffect } from "react";
 import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { removeFav, getNameCharacter } from "./Redux/actions";
-import axios from "axios";
+import { useSelector } from "react-redux";
+
+// REDUX
+
+// LIBRARY
+
 // CSS
 import "./StyleSheets/App.css";
 
-// COMPONENTS
-import Nav from "./Components/Nav";
-import Cards from "./Components/Cards";
-import About from "./Components/About";
-import Detail from "./Components/Detatil";
-import Error from "./Components/Error";
-import Form from "./Components/Form";
-import Favorites from "./Components/Favorites";
+// JAVASCRIP
 
 const App = () => {
   const location = useLocation();
-  const selectUser = useSelector((state) => state.infoUser);
   const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const initialAccess = localStorage.getItem("access") === "true";
-  const [access, setAccess] = useState(initialAccess);
-
-  const [characters, setCharacters] = useState([]);
-  const [email, setEmail] = useState("");
-
-  const onSearch = async (id) => {
-    const URL = `http://localhost:3001/rickandmorty/characters`;
-    try {
-      if (id > 0 || (id < 826 && Number.isInteger(+id))) {
-        const responseApi = await axios(`${URL}/${id}`);
-        const dataClear = responseApi.data;
-        const respuesta = verificarPersonaje(dataClear.id, characters);
-        if (respuesta === true)
-          window.alert("El personaje ya existe no se puede repetir :C");
-        else {
-          if (dataClear.id)
-            setCharacters((oldChars) => [...oldChars, dataClear]);
-          else window.alert("¡No hay personajes con este ID!");
-        }
-      } else {
-        const responseApi = (await axios(`${URL}?name=${id}`)).data;
-        setCharacters((oldChars) => [...oldChars, ...responseApi]);
-      }
-    } catch (error) {
-      alert("¡No hay personajes con este ID!");
-    }
-  };
-
-  const verificarPersonaje = (id, characters) => {
-    let aux = false;
-    for (const i of characters) {
-      if (id === i.id) {
-        aux = true;
-        break;
-      }
-    }
-    return aux;
-  };
-
-  const deletePerson = (id) => {
-    let newId = parseInt(id);
-    const newCharacters = characters.filter(
-      (personajes) => personajes.id !== newId
-    );
-    setCharacters(newCharacters);
-  };
-
-  const onClose = (id, str, idFav) => {
-    str === "bdd"
-      ? dispatch(removeFav(selectUser?.idUser, id)) && deletePerson(idFav)
-      : deletePerson(id);
-  };
-
-  const signUp = async (userData) => {
-    const URL = `http://localhost:3001/rickandmorty/user`;
-    const { email } = userData;
-    try {
-      const { access } = (await axios.post(URL, userData)).data;
-      if (access === true) {
-        //HACER TEST
-        localStorage.setItem("create", "true");
-        setEmail(email);
-        setAccess(true);
-        access && navigate("/home");
-        //HASTA AQUI
-      } else {
-        alert("error");
-      }
-    } catch (error) {
-      alert("error");
-    }
-  };
-  const logout = () => {
-    localStorage.removeItem("access");
-    setAccess(false);
-  };
+  const loginPage = location.pathname === "/";
+  const userData = useSelector((state) => state.infoUser);
+  const accessUser = userData?.access;
 
   useEffect(() => {
-    !access && navigate("/");
-    setCharacters([]);
-  }, [access]);
-
-  useEffect(() => {
-    if (selectUser && selectUser.access === true) {
-      setEmail(email);
-      localStorage.setItem("access", "true");
-      setAccess(true);
-      access && navigate("/home");
+    if (userData && accessUser) {
+      navigate("/home");
+    } else {
+      navigate("/");
     }
-  }, [selectUser]);
+  }, [accessUser]);
 
   return (
     <div className="App">
-      {location.pathname !== "/" && (
-        <Nav onSearch={onSearch} logout={logout} email={email} />
-      )}
+      {!loginPage && <NavBar />}
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route
           path="/home"
-          element={
-            <Cards
-              idUser={selectUser?.idUser}
-              characters={characters}
-              onClose={onClose}
-            />
-          }
+          element={<Home user={userData?.user} idUser={userData?.idUser} />}
         />
-        <Route path="/" element={<Form signUp={signUp} />} />
-        <Route path="/about" element={<About />} />
         <Route path="/detail/:id" element={<Detail />} />
-        <Route path=":id" element={<Error />} />
         <Route
           path="/favorites"
           element={
-            <Favorites idUser={selectUser?.idUser} user={selectUser?.user} />
+            <Favorites idUser={userData?.idUser} user={userData?.user} />
           }
         />
+        <Route path="/about" element={<About />} />
+        <Route path="/user" element={<User idUser={userData?.idUser} />} />
+        <Route path="/error" element={<Error />} />
       </Routes>
     </div>
   );
