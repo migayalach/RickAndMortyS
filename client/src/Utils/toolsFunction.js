aa739702dc187b7d92633a234ce175a9f0924ed7
migayalach <ayalachavezmiguel@gmail.com>
2023-11-30 16:48:23 -0400

    creacion de 2 funciones para poder hacer uso de la libreria swal

--- client/src/Utils/toolsFunction.js
+++ client/src/Utils/toolsFunction.js
@@ -1,3 +1,6 @@
+// LIBRARY
+import Swal from "sweetalert2";
+
 const orderFuc = (array, order, gender, key) => {
   let n = array.length,
     aux = 0,
@@ -26,5 +29,30 @@ const valor = (text) => {
   return character.charCodeAt();
 };
 
-export default orderFuc;
+export const messageUpdateAlert = (message, type) => {
+  Swal.fire({
+    position: "top-end",
+    icon: type,
+    title: message,
+    showConfirmButton: false,
+    timer: 1500,
+  });
+};
 
+export const messageWellcome = (message) => {
+  Swal.fire({
+    title: "Custom width, padding, color, background.",
+    width: 600,
+    padding: "3em",
+    color: "#716add",
+    background: "#fff url(/images/trees.png)",
+    backdrop: `
+      rgba(0,0,123,0.4)
+      url("/images/nyan-cat.gif")
+      left top
+      no-repeat
+    `,
+  });
+};
+
+export default orderFuc;
