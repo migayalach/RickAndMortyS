4e4aca64edcd4808943e9601e408f5fab1a85ab2
migayalach <ayalachavezmiguel@gmail.com>
2023-11-29 20:04:40 -0400

    variable creada

--- client/src/Redux/action-types.js
+++ client/src/Redux/action-types.js
@@ -1,10 +1,10 @@
 export const LOGIN_USER = "LOGIN_USER";
 export const CREATE_USER = "CREATE_USER";
-export const ADD_FAV = "ADD_FAV";
-export const REMOVE_FAV = "REMOVE_FAV";
+export const CHARACTER = "CHARACTER";
+export const REMOVE_CHARACTER = "REMOVE_CHARACTER";
+export const ADD_FAVORITE = "ADD_FAVORITE";
+export const REMOVE_FAVORITE = "REMOVE_FAVORITE";
+export const ALL_FAVORITE = "ALL_FAVORITE";
 export const FILTER_CARDS = "FILTER_CARDS";
-
-export const ORDER_CARDS = "ORDER_CARDS";
-export const GET_CHARACTERS = "GET_CHARACTERS";
-export const GET_ALL_FAVORITES = "GET_ALL_FAVORITES";
-export const GET_NAME_CHARACTER = "GET_NAME_CHARACTER";
+export const ERROR_STATE = "ERROR_STATE";
+export const AUX_STATE = "AUX_STATE";
