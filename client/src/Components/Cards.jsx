5ce53c71d8c856f86ee7a4d9c14d0dde54dfd169
migayalach <ayalachavezmiguel@gmail.com>
2023-11-29 20:12:53 -0400

    paginacion listo

--- client/src/Components/Cards.jsx
+++ client/src/Components/Cards.jsx
@@ -1,54 +1,70 @@
 //HOOK'S
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 //COMPONENT'S
 import Card from "./Card";
 import Pagination from "./Pagination";
 
+// HOOK'S
+
 //STYLESHEETS
 import "../StyleSheets/Cards.css";
 
-const itemsPerPage = 9;
-
-export default function Cards({ characters, onClose }) {
+export default function Cards({ characters, idUser, flag }) {
+  const itemsPerPage = 10;
   const [currentPage, setCurrentPage] = useState(1);
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = characters.slice(indexOfFirstItem, indexOfLastItem);
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
-  const indexOfLastItem = currentPage + itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentRecipes = characters.slice(indexOfFirstItem, indexOfLastItem);
-  useEffect(() => {
-    setCurrentPage(1);
-  }, [characters]);
 
   return (
     <div className="principal">
-      <div className="container">
-        {currentRecipes.map((personaje) => {
-          return (
-            <Card
-              key={personaje.id}
-              id={personaje.id}
-              name={personaje.name}
-              origin={personaje.origin}
-              status={personaje.status}
-              species={personaje.species}
-              gender={personaje.gender}
-              image={personaje.image}
-              onClose={onClose}
-            />
-          );
-        })}
-        <div className="container-pagination">
-          <Pagination
-            dataLength={characters.length}
-            itemsPerPage={itemsPerPage}
-            currentPage={currentPage}
-            onPageChange={handlePageChange}
-          />
-        </div>
+      <div className=" container">
+        {currentItems?.map(
+          (
+            {
+              id,
+              idPerson,
+              name,
+              gender,
+              species,
+              origin: { url },
+              image,
+              status,
+              create,
+            },
+            index
+          ) => {
+            return (
+              <Card
+                key={index}
+                id={id}
+                idPerson={idPerson}
+                name={name}
+                origin={name}
+                url={url}
+                status={status}
+                species={species}
+                gender={gender}
+                image={image}
+                idUser={idUser}
+                create={create}
+                flag={flag}
+              />
+            );
+          }
+        )}
+        <div className="container-pagination"></div>
       </div>
+      <Pagination
+        itemsPerPage={itemsPerPage}
+        totalItems={characters.length}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />{" "}
     </div>
   );
 }
